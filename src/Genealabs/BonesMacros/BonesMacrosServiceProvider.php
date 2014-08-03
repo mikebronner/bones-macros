8f9a5da3cf5474b4774470d37f0cc00798daf91b
Mike Bronner <mike.bronner@genealabs.com>
2014-08-03 09:57:29 -0700

    initial commit

---
+++ src/Genealabs/BonesMacros/BonesMacrosServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Genealabs\BonesMacros;
+
+use Illuminate\Support\ServiceProvider;
+
+class BonesMacrosServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('genealabs/bones-macros');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
