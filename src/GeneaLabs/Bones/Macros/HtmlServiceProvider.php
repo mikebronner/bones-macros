2cdc550751d30a881ae41b7ae1d21468ca7dc72d
Mike Bronner <mike.bronner@genealabs.com>
2014-08-12 14:11:43 -0700

    changed binding

--- src/GeneaLabs/Bones/Macros/HtmlServiceProvider.php
+++ src/GeneaLabs/Bones/Macros/HtmlServiceProvider.php
@@ -40,7 +40,7 @@ class HtmlServiceProvider extends ServiceProvider {
 	 */
 	protected function registerHtmlBuilder()
 	{
-		$this->app->bindShared('html', function($app)
+		$this->app->bind('html', function($app)
 		{
 			return new HtmlBuilder($app['url']);
 		});
@@ -53,7 +53,7 @@ class HtmlServiceProvider extends ServiceProvider {
 	 */
 	protected function registerFormBuilder()
 	{
-		$this->app->bindShared('form', function($app)
+		$this->app->bind('form', function($app)
 		{
 			$form = new FormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
 
