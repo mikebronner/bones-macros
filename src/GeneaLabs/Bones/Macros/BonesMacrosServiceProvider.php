6f0d4ab13dc98f26427b8ae0e0e84104c085f256
Mike Bronner <mike@genealabs.com>
2015-10-05 07:23:43 -0700

    Update service provider

--- src/GeneaLabs/Bones/Macros/BonesMacrosServiceProvider.php
+++ src/GeneaLabs/Bones/Macros/BonesMacrosServiceProvider.php
@@ -1,70 +1,68 @@
 <?php namespace GeneaLabs\Bones\Macros;
 
-class BonesMacrosServiceProvider extends \Illuminate\Html\HtmlServiceProvider {
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Event;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-//	protected $defer = false;
-//
-//    public function boot()
-//    {
-//    	$this->package('genealabs/bones-macros');
-//    }
-//
-//	/**
-//	 * Register the service provider.
-//	 *
-//	 * @return void
-//	 */
-//	public function register()
-//	{
-//	    //$this->unregisterHtmlServiceProvider();
-//		$this->registerHtmlBuilder();
-//		$this->registerFormBuilder();
-//	}
-//
-//	protected function unregisterHtmlServiceProvider()
-//	{
-//
-//	}
+class BonesMacrosServiceProvider extends \Illuminate\Html\HtmlServiceProvider
+{
 
-	/**
-	 * Register the HTML builder instance.
-	 *
-	 * @return void
-	 */
-	protected function registerHtmlBuilder()
-	{
-		$this->app->bindShared('html', function($app)
-		{
-			return new BonesMacrosHtmlBuilder($app['url']);
-		});
-	}
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Register the form builder instance.
-	 *
-	 * @return void
-	 */
-	protected function registerFormBuilder()
-	{
-		$this->app->bindShared('form', function($app)
-		{
-			return new BonesMacrosFormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
-		});
-	}
 
-//	/**
-//	 * Get the services provided by the provider.
-//	 *
-//	 * @return array
-//	 */
-//	public function provides()
-//	{
-//		return array('html', 'form');
-//	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerHtmlBuilder();
+        $this->registerFormBuilder();
+        AliasLoader::getInstance()->alias('Form', BonesMacrosFormFacade::class);
+        AliasLoader::getInstance()->alias('HTML', BonesMacrosHtmlFacade::class);
+    }
 
+    /**
+     * Register the HTML builder instance.
+     *
+     * @return void
+     */
+    protected function registerHtmlBuilder()
+    {
+        $this->app->bindShared('html', function($app)
+        {
+            return new BonesMacrosHtmlBuilder($app['url']);
+        });
+    }
+
+    /**
+     * Register the form builder instance.
+     *
+     * @return void
+     */
+    protected function registerFormBuilder()
+    {
+        $this->app->bindShared('form', function($app)
+        {
+            return new BonesMacrosFormBuilder($app['html'], $app['url'], $app['session.store']->getToken());
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['genealabs-bones-macros'];
+    }
 }
