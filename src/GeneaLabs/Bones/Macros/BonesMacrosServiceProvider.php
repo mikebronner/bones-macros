a84a0cd8ccaa9d91a7d3c24662e366f25523824a
Mike Bronner <mike@genealabs.com>
2016-01-16 14:16:16 -0700

    Update BonesMacrosServiceProvider.php

--- src/GeneaLabs/Bones/Macros/BonesMacrosServiceProvider.php
+++ src/GeneaLabs/Bones/Macros/BonesMacrosServiceProvider.php
@@ -1,12 +1,13 @@
 <?php namespace GeneaLabs\Bones\Macros;
 
+use Collective\Html\HtmlServiceProvider;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Event;
 
-class BonesMacrosServiceProvider extends \Illuminate\Html\HtmlServiceProvider
+class BonesMacrosServiceProvider extends HtmlServiceProvider
 {
 
     /**
